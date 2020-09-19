c6ed4ef49a1c121f287da86d5478bd2ec6b2a66a
KekHawk <55451415+KekHawk@users.noreply.github.com>
2020-09-19 16:59:25 +0100

    Significant improvements to bash file
    Now displays line numbers for tests.
    Removed the need to re-set the current directory within the bash file for every file by getting the files before the loop.

--- tests/RUNALLTESTS.sh
+++ tests/RUNALLTESTS.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-for f in *.py
+#Note: due to line below using 'ls' (unreccomended), files cannot contain spaces. Other special characters are untested.
+files=`ls *.py`
+
+cd C:\\Users\\szymj\\Desktop\\Consume
+
+index=0
+for f in $files
 do
 	cd C:\\Users\\szymj\\Desktop\\Consume
 	f=${f%".py"}
 	
 	if [ "$f" != '__init__' ]
 	then
-		#printf "Running: $f"
+		index=$((index+1))
+		printf "$index "
 		python -m "tests.$f"
 	fi
 done
